ae841034dab1cf1b7543d4b91b3df5cdb28333c5
neripark <neripark@gmail.com>
2021-07-21 16:14:22 +0900

    at v17

--- assets/app.js
+++ assets/app.js
@@ -2,12 +2,3 @@ const test = () => {
   console.log("app init.");
 }
 test();
-
-// ___________________________________________________________________
-//
-const target = document.querySelector("#root");
-const element = React.createElement(
-  "p", {}, "Hello, React."
-);
-
-ReactDOM.render(element, target);
